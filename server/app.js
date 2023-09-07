9c1009284f152d30da361b5d3ed57deaad4e3f65
Mihai <smolenschi.mihai@gmail.com>
2023-09-07 13:57:48 +0300

    add express server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const port = 5500;
+
+const thingsJson = require("./data/things.json");
+const areasJson = require("./data/areas.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/things", (req, res) => {
+  console.log("/things");
+  res.json(thingsJson);
+});
+
+app.get("/areas", (req, res) => {
+  console.log("/areas");
+  res.json(areasJson);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}/`);
+});
